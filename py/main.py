a86a33017b1964d1986decb01a840b18697148a7
adbc <adrianboczon98@gmail.com>
2022-09-07 22:31:13 +0200

    Remove benchmarking related code

--- py/main.py
+++ py/main.py
@@ -1,9 +1,4 @@
-from time import time
 from reader import FSReader
 
 if __name__ == '__main__':
-  start_time = time()
-
   FSReader().create_fs_tree()
-
-  print("--- %s seconds ---" % (time() - start_time))
--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,4 @@
-import process from 'process';
-
 import { FSReader } from './FS/Reader.js';
 
-const start = process.hrtime();
-
 const reader = new FSReader();
-const tree = await reader.createFSTree();
-// tree.printTree();
-
-const stop = process.hrtime(start);
-console.log(`Time Taken to execute: ${(stop[0] * 1e9 + stop[1])/1e9} seconds`);
+await reader.createFSTree();
